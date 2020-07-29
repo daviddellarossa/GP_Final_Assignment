286d4ab18e92683c5eae3e278505dd0f0dbff566
deedeeaar <david.dellarossa@gmail.com>
2020-07-29 08:09:52 +0100

    3DSineGames - BoxGrid animated and camera moving

--- 3DSineGames/sketch.js
+++ 3DSineGames/sketch.js
@@ -1,9 +1,41 @@
+let boxSize = 50;
+let cameraRotationRadius = 800 * 1.414 //800 * sqrt(2) = rotation radius of the camera
+let cameraRotationInitialAngle = 45;
+let cameraRotationSpeed = 0.1;
 function setup() {
     createCanvas(900, 800, WEBGL);
 
+    normalMaterial();
+    stroke(0);
+    strokeWeight(2);
+    angleMode(DEGREES);
+}
+
+function drawBoxGrid(){
+    for(let i = -400; i < 400; i += boxSize){
+        for(let k = -400; k < 400; k += boxSize){
+            push();
+            translate(i, 0, k);
+            let distance = dist(i, 0, k, 0, 0, 0) + frameCount;
+            let length = ((sin(distance) + 1) * 100) + 100;
+            box(boxSize, length, boxSize)
+            pop();
+        }
+    }
+}
+
+function setCameraLocation(){
+
+    let cameraX = cameraRotationRadius * cos(cameraRotationInitialAngle + frameCount * cameraRotationSpeed);
+    let cameraZ = cameraRotationRadius * sin(cameraRotationInitialAngle + frameCount * cameraRotationSpeed);
+    camera (cameraX, -600, cameraZ, 0,0,0, 0,1,0);
 }
 
 function draw() {
     background(125);
-    angleMode(DEGREES);
+
+
+    setCameraLocation();
+
+    drawBoxGrid();
 }
