f3b5d349a1d7107e06a47f040748241b009335d3
deedeeaar <david.dellarossa@gmail.com>
2020-07-31 07:41:22 +0100

    3DSineGames - BoxGrid animated and camera moving Confetti added

--- 3DSineGames/sketch.js
+++ 3DSineGames/sketch.js
@@ -2,6 +2,37 @@ let boxSize = 50;
 let cameraRotationRadius = 800 * 1.414 //800 * sqrt(2) = rotation radius of the camera
 let cameraRotationInitialAngle = 45;
 let cameraRotationSpeed = 0.1;
+let confLocs = [];
+let confTheta = [];
+
+function populateConfettiArrays(){
+    for(let i = 0; i < 200; i++){
+        confLocs.push(
+            createVector(random(-500, 500), random(-800, 0), random(-500, 500))
+        );
+        confTheta.push(random(0, 360));
+    }
+}
+
+function confetti(){
+    for(let i = 0; i < confLocs.length; i++){
+        push();
+        noStroke();
+        translate(confLocs[i]);
+        rotateX(confTheta[i]);
+        rotateZ(confTheta[i]/2);
+        plane(15, 15);
+        pop();
+
+        confTheta[i] += 10;
+
+        confLocs[i].y++;
+        if(confLocs[i].y >= 0){
+            confLocs[i].y = -800;
+        }
+    }
+}
+
 function setup() {
     createCanvas(900, 800, WEBGL);
 
@@ -9,6 +40,8 @@ function setup() {
     stroke(0);
     strokeWeight(2);
     angleMode(DEGREES);
+
+    populateConfettiArrays();
 }
 
 function drawBoxGrid(){
@@ -25,7 +58,6 @@ function drawBoxGrid(){
 }
 
 function setCameraLocation(){
-
     let cameraX = cameraRotationRadius * cos(cameraRotationInitialAngle + frameCount * cameraRotationSpeed);
     let cameraZ = cameraRotationRadius * sin(cameraRotationInitialAngle + frameCount * cameraRotationSpeed);
     camera (cameraX, -600, cameraZ, 0,0,0, 0,1,0);
@@ -33,9 +65,7 @@ function setCameraLocation(){
 
 function draw() {
     background(125);
-
-
     setCameraLocation();
-
     drawBoxGrid();
+    confetti();
 }
