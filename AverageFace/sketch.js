50319ec3756047d995b5b2a54470d4a958628549
deedeeaar <david.dellarossa@gmail.com>
2020-07-31 08:11:41 +0100

    AverageFace initial commit Points 1-4 realized

--- AverageFace/sketch.js
+++ AverageFace/sketch.js
@@ -3,16 +3,25 @@ let avgImg;
 let numOfImages = 30;
 
 //////////////////////////////////////////////////////////
-function preload() { // preload() runs once
-
+async function preload() { // preload() runs once
+    for(let i = 0; i < numOfImages; i++){
+        imgs.push(await loadImage(`assets/${i}.jpg`));
+    }
 }
 //////////////////////////////////////////////////////////
 function setup() {
-    createCanvas(100, 100);
+    createCanvas(imgs[0].width * 2, imgs[0].height);
     pixelDensity(1);
+    avgImg = createGraphics(imgs[0].width, imgs[0].height);
+
 }
 //////////////////////////////////////////////////////////
 function draw() {
     background(125);
+    image(imgs[0], 0, 0);
 
+    for(let i = 0; i < numOfImages; i++){
+        imgs[i].loadPixels();
+    }
+    avgImg.loadPixels();
 }
