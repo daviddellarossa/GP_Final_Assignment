1bbca3a5678e3097976a05ab291e2764b076c0e3
deedeeaar <david.dellarossa@gmail.com>
2020-08-01 17:33:50 +0100

    Code complete

--- AverageFace/sketch.js
+++ AverageFace/sketch.js
@@ -24,4 +24,26 @@ function draw() {
         imgs[i].loadPixels();
     }
     avgImg.loadPixels();
+
+    for(let x = 0; x < imgs[0].width; x++){
+        for(let y = 0; y < imgs[0].height; y++){
+            let pixIdx = (y * imgs[0].width + x) * 4;
+            let sumR = 0;
+            let sumG = 0;
+            let sumB = 0;
+            for(let i = 0; i < imgs.length; i++){
+                sumR += imgs[i].pixels[pixIdx + 0];
+                sumG += imgs[i].pixels[pixIdx + 1];
+                sumB += imgs[i].pixels[pixIdx + 2];
+            }
+            avgImg.pixels[pixIdx + 0] = sumR/imgs.length;
+            avgImg.pixels[pixIdx + 1] = sumG/imgs.length;
+            avgImg.pixels[pixIdx + 2] = sumB/imgs.length;
+            avgImg.pixels[pixIdx + 3] = 255;
+        }
+    }
+    avgImg.updatePixels();
+
+    image(avgImg, imgs[0].width, 0);
+    noLoop();
 }
